b7a56231151960c74dbb408b3413fd170694afea
Umair Shakoor <unicoderone1@gmail.com>
2025-02-08 02:44:15 +0000

    JS Startedgit add .

---
+++ 01. Intro/JS/script.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+console.log("Code is running..");
+console.log("Code is looking like a Wow!");
+console.log("JS is running...");
+
+var a = prompt("Enter Your Number");
+console.log("Your number is " + a);
+
+var w = confirm("Are you sure you wanna leave this page and blast your Computer");
+if (w) {
+    console.log("Your computer is blasting");
+}
+else{
+    console.log("Your computer is not blasting");
+}
+
+document.title = "Hello! JS." ;
+document.body.style.backgroundColor = "red" ;
